e45064c8b8dcb68acad0d130f42c258482b7eacb
Tristan Lins <tristan.lins@infinitysoft.de>
2010-12-14 09:30:04 +0000

    Add runonce.php

---
+++ system/modules/merger2/runonce/runonce.php
@@ -0,0 +1,39 @@
+<?php
+class Merger2Runonce extends Frontend
+{
+
+	/**
+	 * Initialize the object
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+		
+		$this->import('Database');
+	}
+
+	public function run()
+	{
+		$this->import('Database');
+		if ($this->Database->fieldExists('mergerMode', 'tl_module')) {
+			$this->Database->execute("ALTER TABLE tl_module CHANGE mergerMode merger_mode varchar(14) NOT NULL default ''");
+		}
+		if ($this->Database->fieldExists('mergerTemplate', 'tl_module')) {
+			$this->Database->execute("ALTER TABLE tl_module CHANGE mergerTemplate merger_template varchar(64) NOT NULL default 'modulemerger_default'");
+		}
+		if ($this->Database->fieldExists('mergerContainer', 'tl_module')) {
+			$this->Database->execute("ALTER TABLE tl_module CHANGE mergerContainer merger_container char(1) NOT NULL default ''");
+		}
+		if ($this->Database->fieldExists('mergerData', 'tl_module')) {
+			$this->Database->execute("ALTER TABLE tl_module CHANGE mergerData merger_data blob NULL");
+		}
+	}
+}
+
+/**
+ * Instantiate controller
+ */
+$objMerger2Runonce = new Merger2Runonce();
+$objMerger2Runonce->run();
+
+?>
