1aa0183efe8d6e3bec29d16c94a747b24e3354ed
Tristan Lins <tristan.lins@bit3.de>
2013-12-09 14:19:09 +0100

    Add unit and ci tests.

---
+++ tests/bootstrap.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Merger² - Module Merger for Contao Open Source CMS
+ *
+ * Copyright (C) 2013 bit3 UG
+ *
+ * @package merger2
+ * @author  Tristan Lins <tristan.lins@bit3.de>
+ * @link    http://bit3.de
+ * @license LGPL-3.0+
+ */
+
+error_reporting(E_ALL);
+
+function includeIfExists($file)
+{
+	return file_exists($file) ? include $file : false;
+}
+
+if ((!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php')) && (!$loader = includeIfExists(__DIR__.'/../../../autoload.php'))) {
+	echo 'You must set up the project dependencies, run the following commands:'.PHP_EOL.
+		'curl -sS https://getcomposer.org/installer | php'.PHP_EOL.
+		'php composer.phar install'.PHP_EOL;
+	exit(1);
+}
+
+$loader->add('Bit3\Contao\Merger2\Test', __DIR__);
