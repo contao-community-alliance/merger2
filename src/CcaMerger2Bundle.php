4231af34d21b485bdf69115d8e3cf84290705c88
David Molineus <david.molineus@netzmacht.de>
2017-07-05 08:00:16 +0200

    Make it a contao bundle.

---
+++ src/CcaMerger2Bundle.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Merger² - Module Merger for Contao Open Source CMS.
+ *
+ * @copyright 2013,2014 bit3 UG. 2015-2017 Contao Community Alliance
+ * @author    David Molineus <david.molineus@netzmacht.de>
+ *
+ * @link      https://github.com/contao-community-alliance/merger2
+ *
+ * @license   LGPL-3.0+
+ */
+
+namespace ContaoCommunityAlliance\Merger2;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class CcaMerger2Bundle.
+ *
+ * @package ContaoCommunityAlliance\Merger2
+ */
+class CcaMerger2Bundle extends Bundle
+{
+}
---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Merger² - Module Merger for Contao Open Source CMS.
+ *
+ * @copyright 2013,2014 bit3 UG. 2015-2017 Contao Community Alliance
+ * @author    David Molineus <david.molineus@netzmacht.de>
+ *
+ * @link      https://github.com/contao-community-alliance/merger2
+ *
+ * @license   LGPL-3.0+
+ */
+
+namespace ContaoCommunityAlliance\Merger2\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use ContaoCommunityAlliance\Merger2\CcaMerger2Bundle;
+use SunCat\MobileDetectBundle\MobileDetectBundle;
+
+/**
+ * Contao Manager plugin.
+ *
+ * @package ContaoCommunityAlliance\Merger2\ContaoManager
+ */
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(MobileDetectBundle::class),
+            BundleConfig::create(CcaMerger2Bundle::class)
+                ->setReplace('merger2')
+                ->setLoadAfter([ContaoCoreBundle::class, MobileDetectBundle::class])
+        ];
+    }
+}
