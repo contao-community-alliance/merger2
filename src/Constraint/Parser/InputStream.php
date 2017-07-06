7d7082ace30e1b4f22d5cf7655f28fe0032aaadd
David Molineus <david.molineus@netzmacht.de>
2017-07-06 11:50:46 +0200

    Support "-", "_" and "/" for word characters (aliases).

--- src/Constraint/Parser/InputStream.php
+++ src/Constraint/Parser/InputStream.php
@@ -211,7 +211,7 @@ class InputStream
      */
     protected function checkWordCharacter($char)
     {
-        return preg_match('~^[\w<>=!\.]$~', $char);
+        return preg_match('~^[\w<>=!\-\_\/]$~', $char);
     }
 
     /**
