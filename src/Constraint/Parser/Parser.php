6efb95599f91b2f6c9c70b39cfb93394cc35a347
David Molineus <david.molineus@netzmacht.de>
2018-08-06 12:06:24 +0200

    Readd accidentally removed author.

--- src/Constraint/Parser/Parser.php
+++ src/Constraint/Parser/Parser.php
@@ -4,6 +4,7 @@
  * Merger² - Module Merger for Contao Open Source CMS.
  *
  * @package   Merger²
+ * @author    Tristan Lins <tristan.lins@bit3.de>
  * @author    David Molineus <david.molineus@netzmacht.de>
  * @copyright 2013-2014 bit3 UG
  * @copyright 2015-2018 Contao Community Alliance
